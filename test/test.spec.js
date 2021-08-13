bdd80f4d549096a83c4b62887d9785b0f5e08134
crazywook <wook9898@hanmail.net>
2021-08-14 01:51:05 +0900

    test secrets.TEST1

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,7 +1,12 @@
 const { expect } = require('chai')
 
 describe('test.spec', () => {
-  it('Given ', async () => {
-    expect(1).to.be.equal(1)
+  it('Given secrete variable, then receive NODE_ENV and NODE_DEP', async () => {
+    const env = process.env.NODE_ENV
+    const test1 = process.env.TEST1
+    console.log('env', env)
+    console.log('test1', test1)
+    expect(env).to.be.equal('secret-sample1')
+    expect(dep).to.be.equal('secret-sample1')
   }, 600000)
 })
